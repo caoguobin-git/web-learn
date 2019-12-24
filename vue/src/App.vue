8d1ed51e53075570caccee5ac5e4f02cccde3b17
caoguobin <caoguobin@live.com>
2019-12-24 16:47:20 +0800

    去除重复

---
+++ vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <Test/>
+    <NavBar :numlist = 'numList'/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld';
+  import Test from './components/test/Test';
+  import NavBar from "./components/navbar/NavBar";
+
+  export default {
+    name: 'App',
+    data:function(){
+      return {
+        numList:[1,2,3]
+      }
+    },
+    components: {
+      HelloWorld, Test,NavBar
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ vue/src/components/navbar/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="nav-bar">
+    <h1>{{msg}} </h1>
+    <i v-for="item in numlist"> {{item}} </i>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NavBar",
+    props:{
+      numlist:{
+        type:Array,
+        default:function () {
+          return [4,5,6]
+        }
+      }
+    },
+    data: function () {
+      return {msg: '这是一个导航栏'}
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
