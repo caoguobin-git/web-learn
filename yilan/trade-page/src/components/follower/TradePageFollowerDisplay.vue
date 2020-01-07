0177c01a56ebeb22592609c812be8c7a5f119d2e
caoguobin <caoguobin@live.com>
2020-01-07 14:21:13 +0800

    去除重复

--- yilan/trade-page/src/components/follower/TradePageFollowerDisplay.vue
+++ yilan/trade-page/src/components/follower/TradePageFollowerDisplay.vue
@@ -1,40 +1,42 @@
 <template>
-    <div style="width: 98%;margin: auto">
-      <el-menu default-active="2"   mode="horizontal" >
-        <el-menu-item index="1">挂单</el-menu-item>
-        <el-menu-item @click="followerTag='OpenPositions'" index="2">开仓</el-menu-item>
-        <el-menu-item @click="followerTag='ClosedPositions'" index="3">平仓</el-menu-item>
-        <el-menu-item index="4">历史</el-menu-item>
-      </el-menu>
-      <component :is="followerCurrentDisplay"></component>
-    </div>
+  <div style="width: 98%;margin: auto">
+    <el-menu default-active="2" mode="horizontal">
+      <el-menu-item index="1">挂单</el-menu-item>
+      <el-menu-item @click="followerTag='OpenPositions'" index="2">开仓</el-menu-item>
+      <el-menu-item @click="followerTag='ClosedPositions'" index="3">平仓</el-menu-item>
+      <el-menu-item index="4">历史</el-menu-item>
+    </el-menu>
+    <component :is="followerCurrentDisplay"></component>
+  </div>
 </template>
 
 <script>
-    import FollowerOpenPositions from "./FollowerOpenPositions";
-    import FollowerClosedPositions from "./FollowerClosedPositions";
-    export default {
-        name: "TradePageFollowerDisplay",
-      components: {FollowerClosedPositions, FollowerOpenPositions},
-      data(){
-          return{
-            followerTag:'OpenPositions'
-          }
-      },
-      computed:{
-        followerCurrentDisplay(){
-          return 'Follower'+this.followerTag;
-        }
-      }
+  import FollowerOpenPositions from "./FollowerOpenPositions";
+  import FollowerClosedPositions from "./FollowerClosedPositions";
 
+  export default {
+    name: "TradePageFollowerDisplay",
+    components: {FollowerClosedPositions, FollowerOpenPositions},
+    data() {
+      return {
+        followerTag: 'OpenPositions'
+      }
+    },
+    computed: {
+      followerCurrentDisplay() {
+        return 'Follower' + this.followerTag;
+      }
     }
+
+  }
 </script>
 
 <style scoped>
-div{
-  margin: 0;
-}
-  .el-menu-item{
+  div {
+    margin: 0;
+  }
+
+  .el-menu-item {
     height: 25px;
     line-height: 20px;
     font-size: 14px;
