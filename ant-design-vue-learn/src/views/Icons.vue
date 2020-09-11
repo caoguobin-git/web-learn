60d10c2d53f98a51054d4388cd104a5ead607a95
caoguobin <caoguobin@live.com>
2020-09-11 18:03:47 +0800

    Vue learn

---
+++ ant-design-vue-learn/src/views/Icons.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="icons-list">
+      <a-icon type="home"/>
+      <a-icon type="setting" theme="filled"/>
+      <a-icon type="smile" theme="outlined"/>
+      <a-icon type="sync" spin/>
+      <a-icon type="smile" :rotate="180"/>
+      <a-icon type="loading"/>
+    </div>
+    <hr>
+    <div class="icons-list">
+      <a-icon type="step-backward"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Icons"
+}
+</script>
+
+<style scoped>
+.icons-list >>> .anticon {
+  font-size: 24px;
+  margin-right: 6px;
+}
+</style>
